361dd3ac43c206f6435b70b5a61e48e9d00b9037
danya-codit <66479573+danya-codit@users.noreply.github.com>
2020-06-22 19:02:05 +0300

    Add files via upload

---
+++ lec 3 moduie.py
@@ -0,0 +1,40 @@
+a = 3 #atribut modulya
+b = 3
+import lec_3_import
+print ( lec_3_import.a)
+print ( lec_3_import.b)
+
+# b = lek_3_import. a**2
+
+import lec_3_import as gg
+print (gg .a)
+
+# print (gg.a/( 1 - gg.a**3))
+# print()
+# print ( "_____________________________ ")
+# print()
+# print()
+# from gg import a
+# print (a)
+
+# from gg import*
+
+# print (a*b - c)
+
+from astro_constants import ears_mass
+print (ears_mass)
+
+#bibliotece
+
+print()
+print ( "_____________________________ ")
+print()
+print()
+from math import sin, tan, sqrt, pi
+
+print (sin(pi/3))
+print()
+
+print(sqrt(3**2 - 5*sin(pi/2)))
+
+import
---
+++ lek11.py
@@ -0,0 +1,14 @@
+# a="good"
+# b="bad"
+# print(a+b)
+# print(a-b)
+# a=[1,2,3]
+# print(a[1])
+# b=[5,6,7]
+# c=a+b
+# print(c)
+# c=a-b
+# print(c)
+A=[]
+A.append(1)
+print(A)
